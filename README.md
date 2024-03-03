64c99e6a1b24efabf5a9fce22b76fd963f551391
jpcodeplus <behrens.codeplus@gmail.com>
2024-03-03 03:46:48 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tailwind-elements
+
+
+Start intern PHP server 
+```
+php -S localhost:8001  
+```
+
+Start tailwind watcher
+```
+npx tailwindcss -i ./src/style.css -o ./public/style.css --watch
+```
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.{php,html,js,jsx,ts,tsx}", // Für Projekte, die Frameworks wie React oder Vue verwenden
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
